2bbf21e04c3be1d6d514097d4c59d43c627e0cfd
barazeli <barazeli1996>
2020-04-26 14:57:56 +0200

    login and registration finish

---
+++ app/src/main/java/com/barazeli/codelab/MainActivity.java
@@ -0,0 +1,104 @@
+package com.barazeli.codelab;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+
+public class MainActivity extends AppCompatActivity {
+    private Button login,register;
+    private TextInputEditText email_edit,password_edit;
+    private FirebaseAuth auth;
+    private DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setWidgets();
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { setRegister(); }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=email_edit.getText().toString();
+                String password=password_edit.getText().toString();
+                if (validateEmail()||validatePassword()){
+                    setLogin(email,password);
+                
+                }
+            }
+        });
+
+    }
+    private void setWidgets(){
+        login=findViewById(R.id.login_login);
+        register=findViewById(R.id.register_login);
+        email_edit=findViewById(R.id.email_login);
+        password_edit=findViewById(R.id.password_login);
+    }
+    private void setRegister(){
+        Intent intent=new Intent(getApplicationContext(),RegisterActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+    private void setLogin(String email,String password){
+
+      auth=FirebaseAuth.getInstance();
+
+      auth.signInWithEmailAndPassword(email,password)
+              .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+          @Override
+          public void onComplete(@NonNull Task<AuthResult> task) {
+              if (task.isSuccessful()){
+                  Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                  intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                  startActivity(intent);
+                  finish();
+              
+              }else {
+                  Toast.makeText(MainActivity.this, "Problem..",
+                          Toast.LENGTH_SHORT).show();
+
+              }
+          }
+      });
+    }
+    private boolean validateEmail(){
+        String email=email_edit.getText().toString();
+        if (email.isEmpty()){
+            email_edit.setError("Require field ..");
+            email_edit.setFocusable(true);
+            return false;
+        }else {
+            return  true;
+        }
+    }
+    private boolean validatePassword(){
+        String email=password_edit.getText().toString();
+        if (email.isEmpty()){
+            password_edit.setError("Require field ..");
+            password_edit.setFocusable(true);
+            return false;
+        }else {
+            return  true;
+        }
+    }
+}
---
+++ app/src/main/java/com/barazeli/codelab/RegisterActivity.java
@@ -0,0 +1,126 @@
+package com.barazeli.codelab;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+
+import aModel.User;
+
+public class RegisterActivity extends AppCompatActivity {
+    private Button register;
+    private TextInputEditText username_edit,email_edit,password_edit;
+    private FirebaseAuth auth;
+    private FirebaseUser firebaseUser;
+    private DatabaseReference reference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        setWidgets();
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String userName=username_edit.getText().toString();
+                String email=email_edit.getText().toString();
+                String password=password_edit.getText().toString();
+                if (validatePassword()||validateEmail()||validateUserName()){
+                    setRegister(userName,email,password);
+                }
+            }
+        });
+        
+    }
+    private void setWidgets(){
+        register=findViewById(R.id.register_register);
+        username_edit=findViewById(R.id.username_register);
+        email_edit=findViewById(R.id.email_register);
+        password_edit=findViewById(R.id.password_register);
+    }
+    private void setRegister(final String username, String email, String password){
+        auth=FirebaseAuth.getInstance();
+        auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        firebaseUser=auth.getCurrentUser();
+                        assert firebaseUser != null;
+                        String userID=firebaseUser.getUid();
+                        reference= FirebaseDatabase.getInstance()
+                                .getReference("User").child(userID);
+                        HashMap<String,String> hashMap=new HashMap<>();
+                        hashMap.put("id",userID);
+                        hashMap.put("username",username);
+                        hashMap.put("image_url","def");
+                        reference.setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Intent intent=new Intent(getApplicationContext(),HomeActivity.class);
+                                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                                    startActivity(intent);
+                                }else {
+                                    Toast.makeText(RegisterActivity.this,
+                                            "Can't add this user",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+
+                    } else {
+                        Toast.makeText(RegisterActivity.this,
+                                "Authentication Failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                    }
+                });
+    }
+    private boolean validateEmail(){
+        String email=email_edit.getText().toString();
+        if (email.isEmpty()){
+            email_edit.setError("Require field ..");
+            email_edit.setFocusable(true);
+            return false;
+        }else {
+            return  true;
+        }
+    }
+    private boolean validateUserName(){
+        String email=username_edit.getText().toString();
+        if (email.isEmpty()){
+            username_edit.setError("Require field ..");
+            username_edit.setFocusable(true);
+            return false;
+        }else {
+            return  true;
+        }
+    }
+    private boolean validatePassword(){
+        String email=password_edit.getText().toString();
+        if (email.isEmpty()){
+            password_edit.setError("Require field ..");
+            password_edit.setFocusable(true);
+            return false;
+        }else {
+            return  true;
+        }
+    }
+}
+
